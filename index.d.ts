83889dc6c72cb065d83b8723bad436bdbc96992c
Yuriy Apostol <yuriyapostol@gmail.com>
2026-07-25 03:02:26 +0300

    Bump version to 0.1.4 and add index.d.ts for type definitions

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+export type ThemeConfig = any
+export type Sidebar = any
+export type SidebarItem = any
+
+export declare const theme: import('vitepress').Theme
+export declare function defineThemeConfig<T = ThemeConfig>(config: T): T
+export declare function toOpenGraphLocale(locale: string): string
+
+export declare const Comments: any
+export declare function useCommentsConfig(): any
+
+export declare const NewsArchiveLinks: any
+export declare const NewsList: any
+export declare const NewsMeta: any
+export declare const NewsMetaAuthors: any
+export declare const NewsMetaPublishedAt: any
+export declare const NewsMetaStatuses: any
+export declare function formatNewsLabel(...args: any[]): any
+export declare function useNewsConfig(): any
