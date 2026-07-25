1d9df2dfa36f66f09fd358ba97b4935012e3dd1b
Yuriy Apostol <yuriyapostol@gmail.com>
2026-07-25 03:31:09 +0300

    Bump version to 0.1.5 and include comments-news components in theme layout

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,13 +3,24 @@ import type { Theme } from 'vitepress'
 import Layout from './components/Layout.vue'
 import DyvoBadge from './components/DyvoBadge.vue'
 import DyvoUserBadge from './components/DyvoUserBadge.vue'
+import Comments from '../features/comments/Comments.vue'
+import NewsArchiveLinks from '../features/news/NewsArchiveLinks.vue'
+import NewsList from '../features/news/NewsList.vue'
+import NewsMeta from '../features/news/NewsMeta.vue'
+import NewsMetaAuthors from '../features/news/NewsMetaAuthors.vue'
+import NewsMetaPublishedAt from '../features/news/NewsMetaPublishedAt.vue'
+import NewsMetaStatuses from '../features/news/NewsMetaStatuses.vue'
 import { defineComponent, h } from 'vue'
 import './styles/base.css'
 
 const ThemeLayout = defineComponent({
   name: 'DyvoThemeLayout',
   setup(_, { slots }) {
-    return () => h(Layout, null, slots)
+    return () =>
+      h(Layout, null, {
+        ...slots,
+        'doc-after': () => [slots['doc-after']?.(), h(Comments)]
+      })
   }
 })
 
@@ -18,6 +29,12 @@ const theme: Theme = {
   enhanceApp({ app }) {
     app.component('DyvoBadge', DyvoBadge)
     app.component('DyvoUserBadge', DyvoUserBadge)
+    app.component('NewsArchiveLinks', NewsArchiveLinks)
+    app.component('NewsList', NewsList)
+    app.component('NewsMeta', NewsMeta)
+    app.component('NewsMetaAuthors', NewsMetaAuthors)
+    app.component('NewsMetaPublishedAt', NewsMetaPublishedAt)
+    app.component('NewsMetaStatuses', NewsMetaStatuses)
   },
   Layout: ThemeLayout
 }
